9b99a40bb20f89585192fad5b561f24a20bb9516
Igor <iKorobyev@google.com>
2021-06-03 02:20:35 +0300

    Updates

---
+++ precache-manifest.74730385adcb9568c530e6ab239e9ea3.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b683b1001111631ece11ad87979283c3",
+    "url": "/react-graphQL/index.html"
+  },
+  {
+    "revision": "b4c5e93a5c6c1a36249d",
+    "url": "/react-graphQL/static/css/2.d893cde1.chunk.css"
+  },
+  {
+    "revision": "9e401f33b07956f45ec2",
+    "url": "/react-graphQL/static/css/main.6f2fc97b.chunk.css"
+  },
+  {
+    "revision": "b4c5e93a5c6c1a36249d",
+    "url": "/react-graphQL/static/js/2.fe1e8312.chunk.js"
+  },
+  {
+    "revision": "9e401f33b07956f45ec2",
+    "url": "/react-graphQL/static/js/main.932642ea.chunk.js"
+  },
+  {
+    "revision": "beaad2c50a59ac28308b",
+    "url": "/react-graphQL/static/js/runtime~main.1e249740.js"
+  }
+]);
